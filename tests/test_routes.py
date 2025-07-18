c311a94345f90d6f6e406ae97a018b3684f2a850
turdf3rgus0n <rhk7898@yahoo.com>
2025-07-18 00:38:22 -0400

    updates for Issue#2 Read an account from the service

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,39 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+    def test_read_an_account(self):
+        """It should Read an Account"""
+        account = AccountFactory()
+        
+        response = self.client.post(
+            BASE_URL,
+            json=account.serialize(),
+            content_type="application/json"
+        )
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+        new_account = response.get_json()
+
+        response = self.client.get(f"{BASE_URL}/{new_account['id']}")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        read_account = response.get_json()
+        
+        self.assertEqual(read_account["id"], new_account["id"])
+        self.assertEqual(read_account["name"], account.name)
+        self.assertEqual(read_account["email"], account.email)
+        self.assertEqual(read_account["address"], account.address)
+        self.assertEqual(read_account["phone_number"], account.phone_number)
+        self.assertEqual(read_account["date_joined"], str(account.date_joined))
+
+    def test_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        not_found_id = 0
+        response = self.client.get(f"{BASE_URL}/{not_found_id}")
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+
