11212382ce1779fb2c472fd1028e689669bc9c1d
turdf3rgus0n <rhk7898@yahoo.com>
2025-07-19 17:05:47 -0400

    Added security headers

--- service/__init__.py
+++ service/__init__.py
@@ -8,10 +8,17 @@ import sys
 from flask import Flask
 from service import config
 from service.common import log_handlers
+from flask_talisman import Talisman
+
+csp = {
+    'default-src': '\'self\'',
+    'object-src': '\'none\''
+}
 
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
+talisman = Talisman(app, content_security_policy=csp)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
