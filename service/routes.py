b6e835862396573ba99bfe42346f0295b8afc05a
turdf3rgus0n <rhk7898@yahoo.com>
2025-07-19 15:34:42 -0400

    Updates to correct linting errors

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -88,15 +89,15 @@ def read_account(account_id):
     Reads an Account
     This endpoint will read an Account based the account_id that is requested
     """
-    
+
     app.logger.info("Request to read an Account with id: %s", account_id)
-    
+
     account = Account.find(account_id)
-    
+
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
-    return account.serialize(), status.HTTP_200_OK   
+
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
@@ -110,16 +111,16 @@ def update_accounts(account_id):
     This endpoint will update an Account based on the posted data
     """
     app.logger.info("Request to update an Account with id: %s", account_id)
-        
+
     account = Account.find(account_id)
-        
+
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
     account.deserialize(request.get_json())
 
     account.update()
-        
+
     return account.serialize(), status.HTTP_200_OK
 
 
@@ -134,7 +135,7 @@ def delete_accounts(account_id):
     This endpoint will delete an Account based on the account_id that is requested
     """
     app.logger.info("Request to delete an Account with id: %s", account_id)
-    
+
     account = Account.find(account_id)
 
     if account:
